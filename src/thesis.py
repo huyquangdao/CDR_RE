22be4ebc16b669f3526b0b00e43760be1306a5d7
huyquangdao <=>
2021-02-25 10:38:06 +0700

    frist commit in branch develope

---
+++ src/thesis.py
@@ -0,0 +1,13 @@
+"""This is the first module"""
+
+
+def say_hello(text: str) -> None:
+    """[summary]
+
+    Args:
+        text (str): [the string that you want to print in the screen]
+
+    Returns:
+        [str]: [the input string]
+    """
+    print(text)
